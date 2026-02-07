70b18f5aac9e4b1192ad5a561dee4925a4948e8c
chahd hamel <hamelchahd1@gmail.com>
2026-02-07 18:04:58 +0100

    Initial project setup

---
+++ src/main/java/com/green_home_project/GreenHomeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.green_home_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenHomeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenHomeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green_home_project/GreenHomeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green_home_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenHomeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
